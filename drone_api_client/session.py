7f70b88fda1201fdebde2b225b293cf0f0694ca6
m.biriukov <m.biriukov@Elsevier.com>
2021-09-20 22:59:21 +0300

    Added setup

--- drone_api_client/session.py
+++ drone_api_client/session.py
@@ -15,13 +15,13 @@ class Session:
         return self._session.get(f"{host}{url}").json()
 
     def post(self, url: str, data: dict = None, add_repo: bool = True):
-        host = f"{self.__host}/{self.__repo}" if add_repo else self.__host
+        host = f"{self.__host}/repos/{self.__repo}" if add_repo else self.__host
         return self._session.post(f"{host}{url}", data=data).json()
 
     def delete(self, url: str, add_repo: bool = True):
-        host = f"{self.__host}/{self.__repo}" if add_repo else self.__host
+        host = f"{self.__host}/repos/{self.__repo}" if add_repo else self.__host
         return self._session.delete(f"{host}{url}")
 
     def patch(self, url: str, data: dict = None, add_repo: bool = True):
-        host = f"{self.__host}/{self.__repo}" if add_repo else self.__host
+        host = f"{self.__host}/repos/{self.__repo}" if add_repo else self.__host
         return self._session.patch(f"{host}{url}", data=data).json()
